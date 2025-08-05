08a4af428ec67664a2e2fc6d2377cbbc96175fee
MithunR-1998 <mithunsuarez@gmail.com>
2025-08-05 16:44:49 +0530

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,4 +1,14 @@
+import math
 
 def calculateStats(numbers):
-  # implement the computation of statistics here and return the results
-  return None
+  Stats = {}
+  numbers_wihout_nan = [num for num in numbers if not math.isnan(num)]
+  if numbers_wihout_nan:
+    Stats["avg"] = sum(numbers_wihout_nan)/len(numbers_wihout_nan)
+    Stats["max"] = max(numbers_wihout_nan)
+    Stats["min"] = min(numbers_wihout_nan)
+  else:
+    Stats["avg"] = float('nan')
+    Stats["max"] = float('nan')
+    Stats["min"] = float('nan')
+  return Stats
