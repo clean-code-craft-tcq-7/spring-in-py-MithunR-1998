861e8a7eb5c31c2c56cf09a7f57f0d529cd40657
MithunR-1998 <mithunsuarez@gmail.com>
2025-08-05 16:58:25 +0530

    Remove_absurd_values

--- stats.py
+++ stats.py
@@ -1,12 +1,20 @@
 import math
+def remove_absurd_value(numbers):
+  final_list = []
+  for i in numbers:
+    if i>=0 and i <=200:
+      final_list.append(i)
+  return final_list
 
 def calculateStats(numbers):
   Stats = {}
   numbers_wihout_nan = [num for num in numbers if not math.isnan(num)]
-  if numbers_wihout_nan:
-    Stats["avg"] = sum(numbers_wihout_nan)/len(numbers_wihout_nan)
-    Stats["max"] = max(numbers_wihout_nan)
-    Stats["min"] = min(numbers_wihout_nan)
+  final_list_no_absurd = []
+  final_list_no_absurd = remove_absurd_value(numbers_wihout_nan)
+  if final_list_no_absurd:
+    Stats["avg"] = sum(final_list_no_absurd)/len(final_list_no_absurd)
+    Stats["max"] = max(final_list_no_absurd)
+    Stats["min"] = min(final_list_no_absurd)
   else:
     Stats["avg"] = float('nan')
     Stats["max"] = float('nan')
