54699c06139658d11ab691836ef0be2c66774ec4
MithunR-1998 <mithunsuarez@gmail.com>
2025-08-05 16:45:21 +0530

    Update stats.test.py

--- stats.test.py
+++ stats.test.py
@@ -1,5 +1,6 @@
 import unittest
 import stats as statistics
+import math
 
 
 class StatsTest(unittest.TestCase):
@@ -12,11 +13,26 @@ class StatsTest(unittest.TestCase):
 
   def test_avg_is_nan_for_empty_input(self):
     computedStats = statistics.calculateStats([])
+    self.assertTrue(math.isnan(computedStats["avg"]))
+    self.assertTrue(math.isnan(computedStats["max"]))
+    self.assertTrue(math.isnan(computedStats["min"]))
     # All fields of computedStats (average, max, min) must be
     # nan (not-a-number), as defined in the math package
     # Specify the assert here.
     # Use nan and isnan in https://docs.python.org/3/library/math.html
 
+  def test_avg_ignore_nan_inputs(self):
+    computedStats = statistics.calculateStats([1.5, float('nan'), 3.2, 4.5])
+    epsilon = 0.001
+    self.assertAlmostEqual(computedStats["avg"], 3.067, delta=epsilon)
+    self.assertAlmostEqual(computedStats["max"], 4.5, delta=epsilon)
+    self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
+
+  def test_all_values_nan(self):
+    computedStats = statistics.calculateStats([float('nan'),float('nan')])
+    self.assertTrue(math.isnan(computedStats["avg"]))
+    self.assertTrue(math.isnan(computedStats["max"]))
+    self.assertTrue(math.isnan(computedStats["min"]))
 
 if __name__ == "__main__":
   unittest.main()
