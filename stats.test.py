436a26bccee2752dd85eeb23fc337426e4928a9c
MithunR-1998 <mithunsuarez@gmail.com>
2025-08-09 13:29:07 +0530

    Include_additional_tests

--- stats.test.py
+++ stats.test.py
@@ -52,5 +52,58 @@ class StatsTest(unittest.TestCase):
     self.assertAlmostEqual(computedStats["max"], 9.2, delta=epsilon)
     self.assertAlmostEqual(computedStats["min"], 8, delta=epsilon)
 
+  def test_text_inputs_only(self):
+    #text inputs to be removed
+    computedStats = statistics.calculateStats(['a', 'b', 'c', 'z'])
+    self.assertTrue(math.isnan(computedStats["avg"]))
+    self.assertTrue(math.isnan(computedStats["max"]))
+    self.assertTrue(math.isnan(computedStats["min"]))
+
+  def test_text_inputs_with_numbers(self):
+    #text inputs to be removed
+    computedStats = statistics.calculateStats(['a', 'hello', '30', 55, 22.3])
+    epsilon = 0.001
+    self.assertAlmostEqual(computedStats["avg"], 38.65, delta=epsilon)
+    self.assertAlmostEqual(computedStats["max"], 55, delta=epsilon)
+    self.assertAlmostEqual(computedStats["min"], 22.3, delta=epsilon)
+
+  def test_edge_values(self):
+    #range is 0 to 200
+    computedStats = statistics.calculateStats([0, 200])
+    epsilon = 0.001
+    self.assertAlmostEqual(computedStats["avg"], 100, delta=epsilon)
+    self.assertAlmostEqual(computedStats["max"], 200, delta=epsilon)
+    self.assertAlmostEqual(computedStats["min"], 0, delta=epsilon)
+
+  def test_outside_boundary(self):
+    #range is 0 to 200
+    computedStats = statistics.calculateStats([-0.1, 200.01])
+    self.assertTrue(math.isnan(computedStats["avg"]))
+    self.assertTrue(math.isnan(computedStats["max"]))
+    self.assertTrue(math.isnan(computedStats["min"]))
+
+  def test_only_one_invalid(self):
+    #range is 0 to 200
+    computedStats = statistics.calculateStats([200.01])
+    self.assertTrue(math.isnan(computedStats["avg"]))
+    self.assertTrue(math.isnan(computedStats["max"]))
+    self.assertTrue(math.isnan(computedStats["min"]))
+
+  def test_only_one_valid(self):
+    #range is 0 to 200
+    computedStats = statistics.calculateStats([100.5])
+    epsilon = 0.001
+    self.assertAlmostEqual(computedStats["avg"], 100.5, delta=epsilon)
+    self.assertAlmostEqual(computedStats["max"], 100.5, delta=epsilon)
+    self.assertAlmostEqual(computedStats["min"], 100.5, delta=epsilon)
+
+  def test_combination_valid_invalid(self):
+    computedStats = statistics.calculateStats(['a', 'hello', '30', 55, 22.3, 1e12, -5, 999, float('nan'), 0, 200, 75.256])
+    epsilon = 0.001
+    self.assertAlmostEqual(computedStats["avg"], 70.511, delta=epsilon)
+    self.assertAlmostEqual(computedStats["max"], 200, delta=epsilon)
+    self.assertAlmostEqual(computedStats["min"], 0, delta=epsilon)
+
+
 if __name__ == "__main__":
   unittest.main()
